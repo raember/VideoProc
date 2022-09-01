39b11acdbc0a28bb7da7678d0b995c9cb3a924fb
raember <raember@users.noreply.github.com>
2022-09-01 08:36:37 +0200

    Fixed filter

--- to_ogv.sh
+++ to_ogv.sh
@@ -2,6 +2,6 @@
 
 FOLDER='../incoming_to_cut/clear_to_cut/auto_cut'
 
-for f in "$FOLDER"/*; do
-    ffmpeg -i "$f" "${f/.avi/.ogv}" && rm "$f"
+for f in "$FOLDER"/*.avi; do
+    ffmpeg -i "$f" "${f/.avi/.ogv}" -y && rm "$f"
 done
